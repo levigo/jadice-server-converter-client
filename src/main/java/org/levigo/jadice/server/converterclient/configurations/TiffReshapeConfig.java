b44420affc343be98dec9e4451e017569ac405f1
Benjamin Geisselmeier <b.geisselmeier@levigo.de>
2015-01-05 09:41:59 +0100

    Don't use the deprecated ```JadiceToTiffNode``` any longer

--- src/main/java/org/levigo/jadice/server/converterclient/configurations/TiffReshapeConfig.java
+++ src/main/java/org/levigo/jadice/server/converterclient/configurations/TiffReshapeConfig.java
@@ -2,23 +2,22 @@ package org.levigo.jadice.server.converterclient.configurations;
 
 import com.levigo.jadice.server.Job;
 import com.levigo.jadice.server.client.JobFactory;
-import com.levigo.jadice.server.documentplatform.JadiceToTiffNode;
+import com.levigo.jadice.server.documentplatform.ReshapeNode;
+import com.levigo.jadice.server.documentplatform.ReshapeNode.OutputMode;
 import com.levigo.jadice.server.nodes.StreamInputNode;
 import com.levigo.jadice.server.nodes.StreamOutputNode;
 
-@SuppressWarnings("deprecation")
 public class TiffReshapeConfig implements WorkflowConfiguration {
 
 	public Job configureWorkflow(JobFactory jobFactory) throws Exception {
 		Job j = jobFactory.createJob();
-//		JadiceShaperNode tiffShaper = new JadiceShaperNode();
-//		tiffShaper.setOutputMode(OutputMode.JOINED);
-//		tiffShaper.getSettings(TiffConvertConfiguration.class).setRepackingEnabled(false);
-//		tiffShaper.getSettings(TiffConvertConfiguration.class).setRenderAnnotations(true);
-//		tiffShaper.getSettings(TiffConvertConfiguration.class).setCompression(Compression.AUTO);
-//		tiffShaper.getSettings(TiffConvertConfiguration.class).setFontRenderingMode(FontRenderingMode.SHAPE_RENDERING);
+		
+		ReshapeNode tiffShaper = new ReshapeNode();
+		tiffShaper.setTargetMimeType("image/tiff");
+		tiffShaper.setOutputMode(OutputMode.JOINED);
+		
 		j.attach(new StreamInputNode() //
-				.appendSuccessor(new JadiceToTiffNode()) //
+				.appendSuccessor(tiffShaper) //
 				.appendSuccessor(new StreamOutputNode()));
 		return j;
 	}

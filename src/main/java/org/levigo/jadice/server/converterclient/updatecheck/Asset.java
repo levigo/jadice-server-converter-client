c8bc6424c93f612f1a477f4002c1b57ff735c21d
Simon Welsch <s.welsch@levigo.de>
2026-06-30 14:49:17 +0200

    feat(deps): jadice server 5.16.8.2

--- src/main/java/org/levigo/jadice/server/converterclient/updatecheck/Asset.java
+++ src/main/java/org/levigo/jadice/server/converterclient/updatecheck/Asset.java
@@ -2,8 +2,10 @@ package org.levigo.jadice.server.converterclient.updatecheck;
 
 import java.net.URL;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Asset {
 
   public String id;
